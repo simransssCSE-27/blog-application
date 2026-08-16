e06ff22f01fdcfbc0d9dc33e0a6844cb4078dafa
simransssCsE-27 <simranyadav20180093@gmail.com>
2026-08-16 13:49:33 +0530

    Fix Render port configuration

--- backend/server.js
+++ backend/server.js
@@ -5,8 +5,7 @@ const cors = require("cors");
 const apiRoutes = require("./routes/api");
 
 const app = express();
-
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
