15f336a7a480eb3939a31e888385837efce444b2
simransssCsE-27 <simranyadav20180093@gmail.com>
2026-08-18 15:04:51 +0530

    hh

--- backend/server.js
+++ backend/server.js
@@ -20,8 +20,6 @@ app.use(cors({
     allowedHeaders: ["Content-Type", "Authorization"]
 }));
 
-app.options("*", cors());
-
 // ============================
 // MIDDLEWARE
 // ============================
